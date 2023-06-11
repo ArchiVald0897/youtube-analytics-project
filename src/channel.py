aa59c65ad518b5704f4b315ace2a8b1a86536f0e
sergei valdemarov <valdemarovsergei@gmail.com>
2023-06-11 19:12:38 +0300

    Add magical methods

--- src/channel.py
+++ src/channel.py
@@ -16,10 +16,37 @@ class Channel:
         self.channel_i = self.youtube.channels().list(id=self.channel_id, part="snippet,statistics").execute()
         self.title = self.channel_i["items"][0]["snippet"]["title"]
         self.description = self.channel_i["items"][0]["snippet"]["description"]
-        self.url = self.get_channel_self()
-        self.subscribers_count = self.channel_i["item"][0]["statistics"]["subscriberCount"]
-        self.vidio_count = self.channel_i["item"][0]["statistics"]["videoCount"]
-        self.view_count = self.channel_i["item"][0]["statistics"]["viewCount"]
+        self.url = self.channel_i["items"][0]["snippet"]["customUrl"]
+        self.subscribers_count = self.channel_i["items"][0]["statistics"]["subscriberCount"]
+        self.vidio_count = self.channel_i["items"][0]["statistics"]["videoCount"]
+        self.view_count = self.channel_i["items"][0]["statistics"]["viewCount"]
+
+    def __repr__(self):
+        return f"{self.title}(https://www.youtube.com/channel/{self.url})"
+
+    def __str__(self):
+        return f"{self.title}{self.url}"
+
+    def __add__(self, other):
+        """суммируем количество подписчиков"""
+        return int(self.subscribers_count) + int(other.subscribers_count)
+
+    def __sub__(self, other):
+        """вычитае количество подписчиков"""
+        return int(self.subscribers_count) - int(other.subscribers_count)
+
+    def __lt__(self, other):
+        return int(self.subscribers_count) < int(other.subscribers_count)
+
+    def __gt__(self, other):
+        return int(self.subscribers_count) > int(other.subscribers_count)
+
+    def __le__(self, other):
+        return int(self.subscribers_count) <= int(other.subscribers_count)
+
+    def __ge__(self, other):
+        return int(self.subscribers_count) >= int(other.subscribers_count)
+
 
     def print_info(self) -> None:
         """Выводит в консоль информацию о канале в формате JSON"""
@@ -39,6 +66,5 @@ class Channel:
         return f"https://www.youtube.com/channel/{self.channel_id}"
 
     def to_json(self, filename):
-        with open(filename, "w", encoding="utf-8") as f:
-            json.dumps(self.__dict__, f, ensure_ascii=False)
-
+        with open(filename, "w", encoding="utf-8") as file:
+            json.dumps(self.__dict__, file, ensure_ascii=False)
