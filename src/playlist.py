2ac161cbe4fbcd2742818b1cbc85873c3a74e42d
sergei valdemarov <valdemarovsergei@gmail.com>
2023-06-23 23:10:55 +0300

    fixed staticmethod

--- src/playlist.py
+++ src/playlist.py
@@ -11,30 +11,40 @@ class PlayList(YouTubeAPI):
         self.title = playlist_info["items"][0]["snippet"]["title"]
         self.url = f"https://www.youtube.com/playlist?list={self.playlist_id}"
 
+    @staticmethod
+    def duration_to_seconds(duration):
+        """Функция для конвертирования длительности видео в секунды"""
+        duration_str = duration.replace("PT", "").lower()
+        seconds = 0
+        if "h" in duration_str:
+            hours, duration_str = duration_str.split("h")
+            seconds += int(hours) * 3600
+        if "m" in duration_str:
+            minutes, duration_str = duration_str.split("m")
+            seconds += int(minutes) * 60
+        if "s" in duration_str:
+            seconds += int(duration_str[:-1])
+        return seconds
+
     @property
     def total_duration(self):
-        # Получаем информацию о видео в плейлисте
-        playlist_videos = self.youtube.playlistItems().list(playlistId=self.playlist_id,
-                                                            part='contentDetails',
-                                                            maxResults=50,
-                                                            ).execute()
+        """Получаем информацию о видео в плейлисте"""
+        playlist_videos = self.youtube.playlistItems().list(playlistId=self.playlist_id, part='contentDetails',
+                                                            maxResults=50).execute()
         total_seconds = 0
-        # Считаем общую продолжительность всех видео в плейлисте
+        """Считаем общую продолжительность всех видео в плейлисте"""
         for item in playlist_videos["items"]:
             video_id = item["contentDetails"]["videoId"]
-            video_duration = self.youtube.videos().list(id=video_id,
-                                                        part="contentDetails"
+            video_duration = self.youtube.videos().list(id=video_id, part="contentDetails"
                                                         ).execute()["items"][0]["contentDetails"]["duration"]
-            duration_seconds = duration_to_seconds(video_duration)
+            duration_seconds = self.duration_to_seconds(video_duration)
             total_seconds += duration_seconds
         return datetime.timedelta(seconds=total_seconds)
 
     def show_best_video(self):
-        # Находим видео с наибольшим количеством лайков в плейлисте
-        playlist_videos = self.youtube.playlistItems().list(playlistId=self.playlist_id,
-                                                            part='snippet',
-                                                            maxResults=50,
-                                                            ).execute()
+        """Находим видео с наибольшим количеством лайков в плейлисте"""
+        playlist_videos = self.youtube.playlistItems().list(playlistId=self.playlist_id, part='snippet',
+                                                            maxResults=50).execute()
         max_likes = 0
         best_video_url = ""
         for item in playlist_videos["items"]:
@@ -45,19 +55,3 @@ class PlayList(YouTubeAPI):
                 max_likes = likes
                 best_video_url = video.url
         return best_video_url
-
-
-@staticmethod
-def duration_to_seconds(duration):
-    """Функция для конвертирования длительности видео в секунды"""
-    duration_str = duration.replace("PT", "").lower()
-    seconds = 0
-    if "h" in duration_str:
-        hours, duration_str = duration_str.split("h")
-        seconds += int(hours) * 3600
-    if "m" in duration_str:
-        minutes, duration_str = duration_str.split("m")
-        seconds += int(minutes) * 60
-    if "s" in duration_str:
-        seconds += int(duration_str[:-1])
-    return seconds
