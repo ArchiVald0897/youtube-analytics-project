abf94109550e0340581e7aa30a7b0587827f27c4
sergei valdemarov <valdemarovsergei@gmail.com>
2023-06-27 23:40:41 +0300

    exception

--- src/video.py
+++ src/video.py
@@ -1,3 +1,5 @@
+from googleapiclient.errors import HttpError
+
 from src.channel import YouTubeAPI
 
 
@@ -6,11 +8,14 @@ class Video(YouTubeAPI):
 
     def __init__(self, video_id: str):
         self.video_id = video_id
-        self.video_i = self.youtube.videos().list(id=video_id, part="snippet,statistics").execute()
+        try:
+            self.video_i = self.youtube.videos().list(id=video_id, part="snippet,statistics").execute()
+        except HttpError:
+            self.video_i = {"items": [{}]}
 
     @property
     def title(self):
-        return self.video_i["items"][0]["snippet"]["title"]
+        return self.video_i["items"][0]["snippet"]["title"] if self.video_i["items"] else None
 
     @property
     def url(self):
@@ -18,25 +23,28 @@ class Video(YouTubeAPI):
 
     @property
     def view_count(self):
-        return self.video_i["items"][0]["statistics"]["viewCount"]
+        return self.video_i["items"][0]["statistics"]["viewCount"] if self.video_i["items"] else None
 
     @property
     def like_count(self):
-        return self.video_i["items"][0]["statistics"]["likeCount"]
+        return self.video_i["items"][0]["statistics"]["likeCount"] if self.video_i["items"] else None
 
     def __str__(self):
-        return self.title
+        return self.title or ""
 
 
 class PLVideo(Video):
-    """Класс для видио с ютьюб-канала в плейлистах"""
+    """Класс для видео с ютьюб-канала в плейлистах"""
 
     def __init__(self, video_id: str, playlist_id: str):
         super().__init__(video_id)
         self.playlist_id = playlist_id
-        self.playlist_info = self.youtube.playlistItems().list(playlistId=playlist_id,
-                                                               part='contentDetails',
-                                                               maxResults=50).execute()
+        try:
+            self.playlist_info = self.youtube.playlistItems().list(playlistId=playlist_id,
+                                                                   part='contentDetails',
+                                                                   maxResults=50).execute()
+        except HttpError:
+            self.playlist_info = {"items": []}
 
     def __str__(self):
-        return self.title
+        return self.title or ""
